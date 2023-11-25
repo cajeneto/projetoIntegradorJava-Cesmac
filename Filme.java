67b357cbedb89aa9bb3e6da006af893fff954981
Cajé Neto <89559306+cajeneto@users.noreply.github.com>
2023-11-25 17:17:14 -0300

    Filme.java

--- Filme.java
+++ Filme.java
@@ -64,7 +64,7 @@ public class Filme {
             System.out.println("Diretor: Alexandre O Grande");
             System.out.println("Duração: 3:30h");
             System.out.println("Gênero: Comédia");
-            System.out.println("A Trança do Rei Careca é um filme de aventura e fantasia dirigido por Bryce Zabel, com base no popular romance escrito por Lynette Eason. A história se passa no fictício reino de Barlia, onde a coroa foi roubada. Os irmãos Becket, Josh e Courtney, são escolhidos pelo Rei Careca para ajudar a recuperar a coroa.No caminho, eles enfrentam várias provações e obstáculos, como pântanos, terras selvagens e criaturas mágicas. Além disso, eles precisam resolver conflitos e unir a Barlia, que está dividida.Ao longo de sua jornada, os irmãos descobrem a verdade sobre a coroa e os responsáveis pelo roubo. Juntos, eles trabalham para superar as dificuldades e derrotar os inimigos, eventualmente recuperando a coroa e trazendo a unidade e a paz para o reino. A Trança do Rei Careca é uma história emocionante e envolvente que pode ser apreciada por todas as idades.");
+            System.out.println("A Trança do Rei Careca é um filme de aventura e fantasia dirigido por Alexandre O Grande, com base no popular romance escrito por Lynette Eason. A história se passa no fictício reino de Barlia, onde a coroa foi roubada. Os irmãos Becket, Josh e Courtney, são escolhidos pelo Rei Careca para ajudar a recuperar a coroa.No caminho, eles enfrentam várias provações e obstáculos, como pântanos, terras selvagens e criaturas mágicas. Além disso, eles precisam resolver conflitos e unir a Barlia, que está dividida.Ao longo de sua jornada, os irmãos descobrem a verdade sobre a coroa e os responsáveis pelo roubo. Juntos, eles trabalham para superar as dificuldades e derrotar os inimigos, eventualmente recuperando a coroa e trazendo a unidade e a paz para o reino. A Trança do Rei Careca é uma história emocionante e envolvente que pode ser apreciada por todas as idades.");
 
         } else if (d == 3){
             //informa nome do filme
@@ -88,7 +88,7 @@ public class Filme {
             System.out.println("Diretor: Faraó Terceiro");
             System.out.println("Duração: 1:46h");
             System.out.println("Gênero: Comédia");
-            System.out.println("A Volta dos Que Não Foram 2 é um filme brasileiro lançado em 2012. Escrito e dirigido por Beto Brant, o filme conta a história de sete adolescentes que desaparecem em meio a um triângulo amoroso complexo. O enredo se baseia na pergunta de quem será a escolhida no final.");
+            System.out.println("A Volta dos Que Não Foram 2 é um filme brasileiro lançado em 2012. Escrito e dirigido por Faraó Terceiro, o filme conta a história de sete adolescentes que desaparecem em meio a um triângulo amoroso complexo. O enredo se baseia na pergunta de quem será a escolhida no final.");
 
         } else if (d == 4){
             //informa nome do filme
@@ -112,7 +112,7 @@ public class Filme {
             System.out.println("Diretor: Donald Trump");
             System.out.println("Duração: 3:12h");
             System.out.println("Gênero: Comédia");
-            System.out.println("Os quatro cantos das sala redonda é um filme independente brasileiro lançado em 2015. O filme é uma obra de autor dirigida por Hector Babenco e protagonizada por Carla Marins e Ricardo Blat.");
+            System.out.println("Os quatro cantos das sala redonda é um filme independente brasileiro lançado em 2015. O filme é uma obra de autor dirigida por Donald Trump e protagonizada por Carla Marins e Ricardo Blat.");
 
         }
         
