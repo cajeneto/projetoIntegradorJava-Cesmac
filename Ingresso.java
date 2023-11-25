bfbab78bff1750005cf61d7fd210d6bad09da326
Cajé Neto <89559306+cajeneto@users.noreply.github.com>
2023-11-24 23:14:06 -0300

    Add files via upload

---
+++ Ingresso.java
@@ -0,0 +1,11 @@
+public class Ingresso {
+       //# PASSO 3
+    //Crie uma classe Ingresso contendo o filme, tipo de ingresso e o horário da seção.
+
+    String filme;
+    String tipoDeIngresso;
+    String horarioDaSeccao;
+    String compraIngresso;
+
+
+}
